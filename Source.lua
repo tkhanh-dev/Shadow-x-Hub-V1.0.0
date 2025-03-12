4cc4dddcde063d36e6f24831143adbb33eaefcb0
tkhanh-dev <khanhshado9@gmail.com>
2025-03-12 21:59:34 +0700

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tkhanh-dev/sdh/refs/heads/main/Source.lua%20(1).txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tkhanh-dev/sdh/refs/heads/main/Source.lua.txt"))()
